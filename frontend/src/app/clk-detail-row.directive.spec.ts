d6e6aff1c732ef07ba90c0ae788187a5adc0c61b
victorsmits <victor.smits@student.vinci.be>
2019-11-06 20:51:44 +0100

    add directive

---
+++ frontend/src/app/clk-detail-row.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ClkDetailRowDirective } from './clk-detail-row.directive';
+
+describe('ClkDetailRowDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ClkDetailRowDirective();
+    expect(directive).toBeTruthy();
+  });
+});
